3a15576c476f3e8fde6f5ef75732787f9d788ffd
John Carlson <yottzumm@gmail.com>
2024-04-16 09:05:38 -0500

    Checked in a non-working version

---
+++ node/x3d2api.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const fs = require('fs');
+
+let classes = {};
+
+let fileJSON = require('./batchJSON.js').fileJSON;
+
+function processFile(filePath) {
+    let stats = fs.statSync(filePath);
+
+    if (stats.isDirectory()) {
+      traverseDirectory(filePath);
+    } else if (filePath.endsWith('.json')) {
+      fileJSON(classes, 'X3D', filePath);
+    }
+}
+
+function traverseDirectory(directoryPath) {
+  let files = fs.readdirSync(directoryPath);
+
+  files.forEach((file) => {
+    let filePath = directoryPath + '/' + file;
+    processFile(filePath);
+  });
+}
+
+traverseDirectory('C:/x3d-code/www.web3d.org/x3d/content/examples');
+
+for (let clazz in classes) {
+	let classinfo = clazz;
+	classinfo = classinfo.replaceAll("@", "");
+	classinfo = classinfo.replaceAll(":", "_colon_");
+	classinfo = classinfo.replaceAll("-", "");
+	console.log("class", classinfo, "{");
+	for (let method in classes[clazz]) {
+		let methodinfo = classes[clazz][method];
+		methodinfo = methodinfo.replaceAll("@", "");
+		methodinfo = methodinfo.replaceAll(":", "_colon_");
+		methodinfo = methodinfo.replaceAll("-", "");
+		methodinfo = methodinfo.replaceAll("JSON schema", "JSON_schema");
+		console.log(methodinfo);
+	}
+	console.log("}");
+}
